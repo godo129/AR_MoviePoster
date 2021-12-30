05c84bd814f82c0bee161b2f0aafd59a9bc5353f
godo <godo129@naver.com>
2021-12-30 23:50:01 +0900

    포스터 감지

--- AR_MoviePoster/ViewController.swift
+++ AR_MoviePoster/ViewController.swift
@@ -27,6 +27,11 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARImageTrackingConfiguration()
+        
+        if let trackedImages = ARReferenceImage.referenceImages(inGroupNamed: "MoviePosters", bundle: Bundle.main) {
+            configuration.trackingImages = trackedImages
+            configuration.maximumNumberOfTrackedImages = 1
+        }
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -37,6 +42,19 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Pause the view's session
         sceneView.session.pause()
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        let node = SCNNode()
+        
+        if let imageAnchor = anchor as? ARImageAnchor {
+            print(imageAnchor.referenceImage.name)
+            let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
+            let planeNode = SCNNode(geometry: plane)
+            node.addChildNode(planeNode)
+        }
+        
+        return node
     }
 
 }
